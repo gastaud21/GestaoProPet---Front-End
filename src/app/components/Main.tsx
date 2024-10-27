17d37b4cc476c1ab19d848212b5425f336e934cf
Rodrigo Gastaud <rodrigogastaud_97@hotmail.com>
2024-10-27 01:05:35 -0300

    Arrumado o CardAnimal, e o main

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import Link from "next/link";
-import { } from "flowbite";
+import {} from "flowbite";
+import { Just_Me_Again_Down_Here } from "next/font/google";
+import CardAnimal from "./Cards/CardAnimal";
+
+const font = Just_Me_Again_Down_Here({
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 export function Main() {
-    return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p className="text-gray-700 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div className="px-6 pt-4 pb-2">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-        </div>
-    )
+  return (
+    <div className="flex p-7">
+      <div className="flex gap-11">
+        <CardAnimal />
+        <CardAnimal />
+      </div>
+    </div>
+  );
 }
